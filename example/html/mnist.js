3c3e0c4e26689cfdd93b7fea8e7ccd9e058c3959
root <root@berry-love.tk>
2016-03-26 16:23:11 +0900

    fixed an error again^7

--- example/html/mnist.js
+++ example/html/mnist.js
@@ -91,9 +91,9 @@ Main.prototype = {
                     ctx.fillRect(j * 5, i * 5, 5, 5);
                 }
             }
-            /*if (Math.min(...inputs) === 255) {
+            if (Math.min(...inputs) === 255) {
                 return;
-            }*/
+            }
             for (i=0; i<28*28; i++) inputs[i] = 255-inputs[i];
             for (i=0; i<28*28; i++) inputs[i] /= 255;
             a = cat.predict(inputs);
@@ -111,15 +111,15 @@ Main.prototype = {
     }
 }
 
-window.onload = function() {
+/*window.onload = function() {
     var main = new Main();
     $('#clear').click = function() {
         main.initialize();
     }
-}
-/*$(() => {
+}*/
+$(() => {
     var main = new Main();
     $('#clear').click(() => {
         main.initialize();
     });
-});*/
+});
