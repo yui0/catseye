be717ed855a66914fb32e3144a5aa13a0fb9ce2e
Yuichiro Nakada <berry@berry-lab.net>
2021-04-02 23:11:55 +0900

    bug fix

--- _next_generation/espcn_train.c
+++ _next_generation/espcn_train.c
@@ -10,11 +10,12 @@
 #define CATS_USE_ADAM
 #define ADAM_BETA1	0.5
 #define ADAM_BETA2	0.999
-#define ETA		1e-4
+//#define ETA		1e-4
+#define ETA		1e-3
 //#define CATS_USE_RMSPROP
 //#define ETA		0.01	// batch 1
-//#define BATCH		1
-#define BATCH		64
+#define BATCH		1
+//#define BATCH		64
 
 #define CATS_USE_FLOAT
 //#define CATS_OPENCL
@@ -40,26 +41,32 @@ int main()
 #endif
 
 	// https://nykergoto.hatenablog.jp/entry/2019/05/28/%E7%94%BB%E5%83%8F%E3%81%AE%E8%B6%85%E8%A7%A3%E5%83%8F%E5%BA%A6%E5%8C%96%3A_ESPCN_%E3%81%AE_pytorch_%E5%AE%9F%E8%A3%85_/_%E5%AD%A6%E7%BF%92
+	// https://qiita.com/nekono_nekomori/items/08ec250ceb09a0004768
 	CatsEye_layer u[] = {
 		{size/4, CATS_CONV, ETA, .ksize=5, .stride=1, .padding=2, .ch=64, .ich=3 },
-		{     0, CATS_ACT_TANH },
+//		{     0, CATS_ACT_TANH },
+		{     0, CATS_ACT_RELU },
 
-		{     0, CATS_CONV, ETA, .ksize=3, .stride=1, .padding=1, .ch=64 },
-		{     0, CATS_ACT_TANH },
+/*		{     0, CATS_CONV, ETA, .ksize=3, .stride=1, .padding=1, .ch=64 },
+//		{     0, CATS_ACT_TANH },
+		{     0, CATS_ACT_RELU },*/
 
 		{     0, CATS_CONV, ETA, .ksize=3, .stride=1, .padding=1, .ch=32 },
-		{     0, CATS_ACT_TANH },
+//		{     0, CATS_ACT_TANH },
+		{     0, CATS_ACT_RELU },
 
-		{     0, CATS_CONV, ETA, .ksize=1, .stride=1, .ch=4*3 },
+		{     0, CATS_CONV, ETA, .ksize=3, .stride=1, .padding=1, .ch=4*3 },
+//		{     0, CATS_CONV, ETA, .ksize=1, .stride=1, .ch=4*3 },
 //		{     0, CATS_ACT_TANH }, // avoid nan
-		{     0, CATS_PIXELSHUFFLER, .r=2, .ch=3 },
+		{     0, CATS_PIXELSHUFFLER, .r=2, .ch=3, .name="Output" },
 
 		{  size, CATS_LOSS_IDENTITY_MSE },
 	};
 	CatsEye cat = { .batch=BATCH };
 	CatsEye__construct(&cat, u);
+	int output = CatsEye_getLayer(&cat, "Output");
 
-	real *xh = calloc(sizeof(real), k/2*k/2*sample);	// 訓練データ
+	real *xh = calloc(sizeof(real), k/2*k/2*3*sample);	// 訓練データ
 	int16_t t[sample];				// ラベルデータ
 	//uint8_t *data = malloc(sample*size);
 
@@ -71,7 +78,7 @@ int main()
 	}
 	printf("OK\n");
 	/*{
-	unsigned char *pixels = calloc(3, size*100);
+	uint8_t *pixels = calloc(3, size*100);
 	for (int i=0; i<10*10; i++) {
 		CatsEye_visualize(xh+size/4*i, k/2*k/2, k/2, &pixels[((i/10)*k/2*k*10+(i%10)*k/2)*3], k*10, 3);
 		CatsEye_visualize(x+size*i, k*k, k, &pixels[((i/10)*k*k*10+(i%10)*k)*3], k*10, 3);
@@ -82,64 +89,23 @@ int main()
 	// 訓練
 	printf("Starting training...\n");
 //	CatsEye_train(&cat, xh, x, sample, 10/*repeat*/, sample/*random batch*/, sample/10);
-	CatsEye_train(&cat, xh, x, sample, 45/*repeat*/, sample/*random batch*/, 0);
+//	CatsEye_train(&cat, xh, x, sample, 45/*repeat*/, sample/*random batch*/, 0);
+	CatsEye_train(&cat, xh, x, sample, 5/*repeat*/, sample/*random batch*/, 0);
 	printf("Training complete\n");
 
 	// 結果の表示
 	static int result[10][10];
-	unsigned char *pixels = calloc(1, size*100);
+	uint8_t *pixels = calloc(3, size*100);
 	int c = 0;
 	int r = 0;
-	for (int i=0; i<sample; i++) {
-		int p = CatsEye_predict(&cat, x+size*i);
-		result[t[i]][p]++;
-		if (p==t[i]) r++;
-		else {
-			if (c<100) {
-				CatsEye_visualize(x+size*i, k*k, k, &pixels[(c/10)*size*10+(c%10)*k*3], k*10, 3);
-			}
-			c++;
-		}
-	}
-	for (int i=0; i<10; i++) {
-		for (int j=0; j<10; j++) {
-			printf("%3d ", result[i][j]);
-		}
-		printf("\n");
-	}
-	printf("Prediction accuracy on training data = %f%%\n", (float)r/sample*100.0);
-	stbi_write_png(NAME"_wrong.png", k*10, k*10, 3/*bpp*/, pixels, 0);
-
-	int n[10]; // 10 classes
-	memset(n, 0, sizeof(int)*10);
-	memset(pixels, 0, size*100);
 	for (int i=0; i<10*10; i++) {
-		int p = CatsEye_predict(&cat, x+size*i);
+		CatsEye_forward(&cat, xh+k/2*k/2*3*i);
 
-		CatsEye_visualize(x+size*i, k*k, k, &pixels[(p*k*k*10+(n[p]%10)*k)*3], k*10, 3);
-		n[p]++;
+//		CatsEye_visualize(xh+size/4*i, k/2*k/2, k/2, &pixels[((i/10)*k/2*k*10+(i%10)*k/2)*3], k*10, 3);
+//		CatsEye_visualize(x+size*i, k*k, k, &pixels[((i/10)*k*k*10+(i%10)*k)*3], k*10, 3);
+		CatsEye_visualize(cat.layer[output].z, k*k, k, &pixels[((i/10)*k*k*10+(i%10)*k)*3], k*10, 3);
 	}
-	stbi_write_png(NAME"_classify.png", k*10, k*10, 3, pixels, 0);
-
-	memset(pixels, 0, size*100);
-	/*for (int i=0; i<10*10; i++)*/ {
-		int p = CatsEye_predict(&cat, x/*+size*i*/);
-
-		int x = 0;
-		for (int n=0; n<cat.layers; n++) {
-			CatsEye_layer *l = &cat.layer[n];
-			if (l->type == CATS_LINEAR) {
-				continue;
-			}
-
-			int mch = l->ch > 10 ? 10 : l->ch;
-			for (int ch=0; ch<mch; ch++) {
-				CatsEye_visualize(&l->z[ch*l->ox*l->oy], l->ox*l->oy, l->ox, &pixels[x +ch*(l->oy+2)*k*10], k*10, 1);
-			}
-			x += l->ox+2;
-		}
-	}
-	stbi_write_png(NAME"_predict.png", k*10, k*10, 1, pixels, 0);
+	stbi_write_png(NAME"_.png", k*10, k*10, 3, pixels, 0);
 	free(pixels);
 
 	free(x);
